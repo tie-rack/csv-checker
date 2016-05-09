30a57f3f16001981bdf6bb8a11a60b28f51f6bb9
Chris Shea <cmshea@gmail.com>
2016-05-09 17:27:12 -0400

    Move logic of each byte to fns for current state

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ const CR: u8 = 13;
 const QUOTE: u8 = 34;
 const COMMA: u8 = 44;
 
+#[derive(PartialEq)]
 enum CSVState {
     Start,
     NonQuotedValue,
@@ -16,10 +17,73 @@ enum CSVState {
     Error,
 }
 
+type CSVResult = Result<CSVState, ()>;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct CSVError {
     pub line: i32,
     pub col: i32,
+}
+
+fn parse_start(byte: u8) -> CSVResult {
+    match byte {
+        QUOTE => Ok(CSVState::QuotedValue),
+        COMMA => Ok(CSVState::Start),
+        _ => Ok(CSVState::NonQuotedValue)
+    }
+}
+
+fn parse_non_quoted(byte: u8) -> CSVResult {
+    match byte {
+        COMMA => Ok(CSVState::Start),
+        CR    => Ok(CSVState::ExpectLF),
+        LF    => Ok(CSVState::Start),
+        QUOTE => Ok(CSVState::NonQuotedQuote),
+        _     => Ok(CSVState::NonQuotedValue),
+    }
+}
+
+fn parse_non_quoted_quote(byte: u8) -> CSVResult {
+    match byte {
+        QUOTE => Ok(CSVState::NonQuotedValue),
+        COMMA => Err(()),
+        CR    => Err(()),
+        LF    => Err(()),
+        _     => Ok(CSVState::NonQuotedQuote),
+    }
+}
+
+fn parse_quoted(byte: u8) -> CSVResult {
+    match byte {
+        QUOTE => Ok(CSVState::QuoteQuote),
+        CR    => Err(()),
+        LF    => Err(()),
+        _     => Ok(CSVState::QuotedValue),
+    }
+}
+
+fn parse_quote_quote(byte: u8) -> CSVResult {
+    match byte {
+        QUOTE => Ok(CSVState::QuotedValue),
+        COMMA => Ok(CSVState::Start),
+        CR    => Ok(CSVState::ExpectLF),
+        LF    => Ok(CSVState::Start),
+        _     => Err(()),
+    }
+}
+
+fn parse_cr(byte: u8) -> CSVResult {
+    match byte {
+        LF => Ok(CSVState::Start),
+        _  => Err(()),
+    }
+}
+
+fn parse_err(byte: u8) -> CSVResult {
+    match byte {
+        LF => Ok(CSVState::Start),
+        _  => Ok(CSVState::Error),
+    }
 }
 
 pub fn errors_for_csv(file: File) -> Vec<CSVError> {
@@ -32,99 +96,22 @@ pub fn errors_for_csv(file: File) -> Vec<CSVError> {
     for byte in file.bytes() {
         let b = byte.unwrap();
 
-        match (&state, b) {
-            // At the start of a field, are we a quoted field, non-quoted, or empty?
-            (&CSVState::Start, QUOTE) => {
-                state = CSVState::QuotedValue;
-            },
-            (&CSVState::Start, COMMA) => {
-                state = CSVState::Start
-            },
-            (&CSVState::Start, _) => {
-                state = CSVState::NonQuotedValue;
-            },
+        let next_state = match state {
+            CSVState::Start          => parse_start(b),
+            CSVState::NonQuotedValue => parse_non_quoted(b),
+            CSVState::NonQuotedQuote => parse_non_quoted_quote(b),
+            CSVState::QuotedValue    => parse_quoted(b),
+            CSVState::QuoteQuote     => parse_quote_quote(b),
+            CSVState::ExpectLF       => parse_cr(b),
+            CSVState::Error          => parse_err(b),
+        };
 
-            // In a non-quoted field, watch for quotes
-            (&CSVState::NonQuotedValue, COMMA) => {
-                state = CSVState::Start;
-            }
-            (&CSVState::NonQuotedValue, CR) => {
-                state = CSVState::ExpectLF;
-            }
-            (&CSVState::NonQuotedValue, LF) => {
-                state = CSVState::Start;
-            }
-            (&CSVState::NonQuotedValue, QUOTE) => {
-                state = CSVState::NonQuotedQuote;
-            }
-
-            // A quote in a non-quoted field needs a matching quote to
-            // close it out.
-            (&CSVState::NonQuotedQuote, QUOTE) => {
-                state = CSVState::NonQuotedValue;
-            }
-            (&CSVState::NonQuotedQuote, COMMA) => {
+        state = match next_state {
+            Ok(new_state) => new_state,
+            Err(_)        => {
                 errors.push(CSVError { line: line, col: col });
-                state = CSVState::Error;
-            }
-            (&CSVState::NonQuotedQuote, CR) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Start;
-            }
-            (&CSVState::NonQuotedQuote, LF) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Start;
-            }
-
-            // In a quoted field, watch for quotes or newlines
-            (&CSVState::QuotedValue, QUOTE) => {
-                state = CSVState::QuoteQuote;
+                CSVState::Error
             },
-            (&CSVState::QuotedValue, CR) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Start;
-            },
-            (&CSVState::QuotedValue, LF) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Start;
-            },
-
-            // A quote in a quote needs a quote immediately following
-            // to be an escaped quote. If there's a comma or newline
-            // following, it means that the field is done. Otherwise,
-            // it's an error.
-            (&CSVState::QuoteQuote, QUOTE) => {
-                state = CSVState::QuotedValue;
-            },
-            (&CSVState::QuoteQuote, COMMA) => {
-                state = CSVState::Start;
-            },
-            (&CSVState::QuoteQuote, CR) => {
-                state = CSVState::ExpectLF;
-            },
-            (&CSVState::QuoteQuote, LF) => {
-                state = CSVState::Start;
-            },
-            (&CSVState::QuoteQuote, _) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Error;
-            },
-
-            // If we're looking at CR, we need a LF to follow
-            (&CSVState::ExpectLF, LF) => {
-                state = CSVState::Start;
-            },
-            (&CSVState::ExpectLF, _) => {
-                errors.push(CSVError { line: line, col: col });
-                state = CSVState::Error;
-            },
-
-            // If we're in an error state, once we reach a newline, we
-            // start over.
-            (&CSVState::Error, LF) => {
-                state = CSVState::Start;
-            },
-            _ => ()
         };
 
         if b == LF {
